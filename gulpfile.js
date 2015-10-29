5f9299e0bc8bfee7f42629a8f53f01be9a0170af
Eduard Tkachuk <etkach@softserveinc.com>
2015-10-29 17:05:08 +0200

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var typescript = require('gulp-tsc');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify'); 
+ 
+gulp.task('tsc', function(){
+	return gulp.src('./app/src/**/*.ts')
+		.pipe(typescript())
+		.pipe(gulp.dest('./tmp/'))
+		.pipe(concat('app.js'))
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('tsc:prod', function(){
+	return gulp.src('./app/src/**/*.ts')
+		.pipe(typescript())
+		.pipe(gulp.dest('./tmp/'))
+		.pipe(concat('app.js'))
+		.pipe(uglify())
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('./tmp/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('index', function () {
+    return gulp.src('./app/index.html')
+        .pipe(gulp.dest('./build'))
+});
+
+gulp.task('dev', ['tsc', 'index']);
+gulp.task('release', ['tsc:prod', 'index']);
+
+gulp.task('watch', ['dev'], function () {
+    gulp.watch('./app/**', ['dev']);
+});
