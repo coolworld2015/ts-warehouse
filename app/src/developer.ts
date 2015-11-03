b87ad93ae040d10d11033cb8348e7aaa7db0fc63
wintermute2015 <wintermute2015@ukr.net>
2015-11-03 15:42:06 +0200

    modified test.ts

---
+++ app/src/developer.ts
@@ -0,0 +1,10 @@
+class Developer {
+	private name: string;
+	constructor(name: string) {
+		this.name = name;
+	}
+	sayHello() {
+		return 'Dev name is ' + this.name;
+	}
+}
+var ed = new Developer('Edward');
--- app/src/test.ts
+++ app/src/test.ts
@@ -1,11 +1,52 @@
-var num1: number = 1;
-class Developer {
-	private name: string;
-	constructor(name: string) {
-		this.name = name;
+module types {
+	//declare let angular;
+	let any1 = 'angular';
+
+	let num1: number = 1;
+	let hasCheck: boolean = true;
+
+	let name: string = 'ed';
+	let names: string[] = ['ed', 'john'];
+
+	let empty: any = null;
+
+	let person = {
+		id: num1,
+		name: name,
+		sayHelloOld: function() {
+			return console.log(this.name);
+		},
+		sayHello: () => console.log('Cool')
+	};
+	person.sayHello();
+
+	let doSomething: Function = () => console.log(any1);
+	doSomething();
+
+	let addTwo = (a: number, b: number) => a + b;
+	console.log(addTwo(2,3));
+
+	interface TestInterface {
+		(x: number): number;
 	}
-	sayHello() {
-		return 'Dev name is ' + this.name;
+
+	let testInterface: TestInterface =
+		(num) => (num * num);
+	console.log(testInterface(5));
+
+	interface Developer {
+		name: string;
+		skills: string[];
+		sayHello: () => string;
 	}
+
+	let ed: Developer = {
+		name: 'ed',
+		skills: ['js', ' html'],
+		sayHello: function() {
+			return this.name + ' has skills: ' +this.skills;
+		}
+	};
+	console.log(ed.sayHello());
+    ed.skills
 }
-var ed = new Developer('Edward');
