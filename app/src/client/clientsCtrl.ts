b77ffe6247e6a0cda5ef246ad45129faa7a49b7a
wintermute2015 <wintermute2015@ukr.net>
2015-11-04 18:54:40 +0200

    added module app.clients

--- app/src/client/clientsCtrl.ts
+++ app/src/client/clientsCtrl.ts
@@ -1,36 +1,26 @@
 /// <reference path='../common/lib/angular.d.ts' />
   
-function ClientsCtrl_js() {
-	var vm = this;
-	
-	vm.title = 'TypeScript'; 
-	
-	angular.extend(vm, {
-		init: init
-	});
-
-	function init() {
-		vm.title = 'CoolWorldJS';
-	}		
-}		
-
-interface IClientsCtrl {
-	title: string;
-	init(): void;
-}
-
-class ClientsCtrl implements IClientsCtrl{
-	title: string;
-	
-	constructor() {
-		this.title = 'TypeScript'; 
+module app.clients {  
+	interface IClientsCtrl {
+		title: string;
+		init(): void;
 	}
-	
-	init(): void {
-		this.title = 'CoolWorld';
-	};
-}
 
-angular
-	.module('app')
-	.controller('ClientsCtrl', ClientsCtrl);
+	class ClientsCtrl implements IClientsCtrl{
+		title: string;
+		
+		constructor() {
+			this.title = 'TypeScript'; 
+		}
+		
+		init(): void {
+			this.title = 'CoolWorld';
+		};
+	}
+
+	angular
+		.module('app')
+		.controller('ClientsCtrl', ClientsCtrl);
+	
+}
+ 
