f3453347c7276dd6ebdd3e85ac946ede6a4b364f
wintermute2015 <wintermute2015@ukr.net>
2015-11-05 14:00:58 +0200

    added class Product

--- app/src/client/clientsCtrl.ts
+++ app/src/client/clientsCtrl.ts
@@ -1,23 +1,32 @@
 /// <reference path='../common/lib/angular.d.ts' />
   
 module app.clients {  
-	interface IClientsCtrl {
+	interface IClient {
 		title: string;
 		init(): void;
+		products: app.product.IProduct[];
 	}
 
-	class ClientsCtrl implements IClientsCtrl{
+	class ClientsCtrl implements IClient{
 		title: string;
+		products: app.product.IProduct[];
 		
 		constructor() {
 			this.title = 'TypeScript'; 
-			//this.init();
+			this.products = [
+				{
+					"name": "item1",
+					"price": 555
+				}
+			]; 
+			this.init();
 		}
 		
 		init(): void {
 			this.title = 'CoolWorld';
+			let newProduct = new app.product.Product("item2", 333);
+			this.products.push(newProduct);			
 		};
-
 	}
 
 	angular
@@ -25,4 +34,22 @@ module app.clients {
 		.controller('ClientsCtrl', ClientsCtrl);
 	
 }
- 
+
+module app.product {  
+	export interface IProduct {
+		name: string;
+		price: number;
+		//showName(): string;
+	} 
+
+	export class Product implements IProduct{
+	
+		constructor(public name: string,
+					public price: number) {
+		}
+		
+		showName(): string {
+			return this.name;
+		}
+	}
+}	
