4da32e68d6f8fd7b34006997fc8a0203820e60b2
wintermute2015 <wintermute2015@ukr.net>
2015-11-05 16:02:38 +0200

    added class ClientsService

--- app/src/client/clientsCtrl.ts
+++ app/src/client/clientsCtrl.ts
@@ -4,28 +4,35 @@ module app.clients {
 	interface IClient {
 		title: string;
 		init(): void;
-		products: app.product.IProduct[];
+		setClients(clientsService): void;
+		clients: any[];
 	}
 
 	class ClientsCtrl implements IClient{
-		title: string;
-		products: app.product.IProduct[];
+		static $inject = ['ClientsService'];
 		
-		constructor() {
-			this.title = 'TypeScript'; 
-			this.products = [
-				{
-					"name": "item1",
-					"price": 555
-				}
-			]; 
-			this.init();
+		title: string;
+		clients: any[];
+		
+		constructor(clientsService) {
+			this.clients = clientsService.getClients();
+			this.title = 'TypeScript';
+ 
+			//this.init();
+			//this.setClients();
 		}
 		
 		init(): void {
 			this.title = 'CoolWorld';
 			let newProduct = new app.product.Product("item2", 333);
-			this.products.push(newProduct);			
+			this.clients.splice(0,10);
+			console.log(this.clients)
+			this.clients.push(newProduct);
+		}; 
+		
+		setClients(clientsService): void {
+				console.log(clientsService)
+			//this.clients = clientsService.getClients();
 		};
 	}
 
@@ -35,11 +42,25 @@ module app.clients {
 	
 }
 
+module app.data {  
+	class ClientsService{
+		getClients() {
+			let clients = localStorage.getItem('warehouse_clients');
+			clients = JSON.parse(clients);
+			return clients.splice(0,10);
+		}	
+	} 
+	
+	angular
+		.module('app')
+		.service('ClientsService', ClientsService);	
+}
+
 module app.product {  
 	export interface IProduct {
 		name: string;
 		price: number;
-		//showName(): string;
+		showName(): string; 
 	} 
 
 	export class Product implements IProduct{
